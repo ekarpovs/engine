93d2556bd7fcb9760fc78e30a1345fd082920fcd
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-03-04 15:39:01 +0200

    Add documentation for ws-aux module

--- project/modules/ws-aux.py
+++ project/modules/ws-aux.py
@@ -1,9 +1,27 @@
+'''
+Local module, contains common utilites
+'''
 import cv2
 
+import sys
+import types
+
 def empty(**kwargs):
+  '''
+  It is an operation's implementation boilerplate.
+  Gets kwargs, do nothing and returns the kwargs as is.
+  '''  
+
   return kwargs
 
 def store(**kwargs):
+  '''
+  Stores an image into file.
+  Gets via kwargs: 
+    - image that will be stored;
+    - full file name, where the image will be stored
+  Returns the kwargs as is.
+  '''  
 
   image = kwargs.get('image', None)
   ffn = kwargs.get('ffn', '')
@@ -14,12 +32,20 @@ def store(**kwargs):
   return kwargs
 
 def clean(**kwargs):
+  '''
+  Cleans kwargs dictionary from items.
+  Returns the kwargs with only 'exec', 'image', 'brk' and 'show' items.
+  '''  
+
   const_keys = ['exec', 'image', 'brk', 'show']
   for key in [k for k in kwargs if k not in const_keys]: kwargs.pop(key, None)
 
   return kwargs
 
 def printkwargs(**kwargs):
+  '''
+  Prints  kwargs and returns the kwargs as is.
+  '''  
   [print(k, v) for k, v in kwargs.items() if k != "image"]
   
   return kwargs
