0473c24e216c5c07510507a2d6a903df2ef699ff
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-03-05 12:09:44 +0200

    Change doc string format for md compatible

--- project/modules/ws-aux.py
+++ project/modules/ws-aux.py
@@ -1,5 +1,5 @@
 '''
-Local module, contains common utilites
+Local module, contains common utility operations
 '''
 import cv2
 
@@ -9,18 +9,26 @@ import types
 def empty(**kwargs):
   '''
   It is an operation's implementation boilerplate.
-  Gets kwargs, do nothing and returns the kwargs as is.
+
+  Keyword arguments:
+  - image: an image that will be returned
+
+  Returns:
+  - the kwargs as is.
   '''  
 
   return kwargs
 
 def store(**kwargs):
   '''
-  Stores an image into file.
-  Gets via kwargs: 
-    - image that will be stored;
-    - full file name, where the image will be stored
-  Returns the kwargs as is.
+  Stores an image into a file.
+  
+  Keyword arguments:
+  - image: an image that will be stored;
+  - ffn: full file name, where the image will be stored.
+  
+  Returns:
+  - the kwargs as is.
   '''  
 
   image = kwargs.get('image', None)
@@ -34,7 +42,12 @@ def store(**kwargs):
 def clean(**kwargs):
   '''
   Cleans kwargs dictionary from items.
-  Returns the kwargs with only 'exec', 'image', 'brk' and 'show' items.
+  
+  Keyword arguments:
+  - image: an image that will be returned
+
+  Returns:
+  - the kwargs with only 'exec', 'image', 'brk' and 'show' items.
   '''  
 
   const_keys = ['exec', 'image', 'brk', 'show']
@@ -44,8 +57,15 @@ def clean(**kwargs):
 
 def printkwargs(**kwargs):
   '''
-  Prints  kwargs and returns the kwargs as is.
-  '''  
+  Prints kwargs.
+
+  Keyword arguments:
+  - image: an image that will be returned
+
+  Returns:
+  - the kwargs as is.
+  '''
+
   [print(k, v) for k, v in kwargs.items() if k != "image"]
   
   return kwargs
