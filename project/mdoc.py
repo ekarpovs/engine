2e6ca005f75cf9d6216c7495391006bc7ce3ca82
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-03-06 10:33:21 +0200

    Add trim function for docstring

--- project/mdoc.py
+++ project/mdoc.py
@@ -19,6 +19,33 @@ def parseArgs():
   return kwargs
 
 
+def trim(docstring):
+  if not docstring:
+      return ''
+  # Convert tabs to spaces (following the normal Python rules)
+  # and split into a list of lines:
+  lines = docstring.expandtabs().splitlines()
+  # Determine minimum indentation (first line doesn't count):
+  maxint = 4
+  indent = maxint
+  for line in lines[1:]:
+      stripped = line.lstrip()
+      if stripped:
+          indent = min(indent, len(line) - len(stripped))
+  # Remove indentation (first line is special):
+  trimmed = [lines[0].strip()]
+  if indent < maxint:
+      for line in lines[1:]:
+          trimmed.append(line[indent:].rstrip())
+  # Strip off trailing and leading blank lines:
+  while trimmed and not trimmed[-1]:
+      trimmed.pop()
+  while trimmed and not trimmed[0]:
+      trimmed.pop(0)
+  # Return a single string:
+  return '\n'.join(trimmed)
+
+
 def make_doc(path):
   doc = []
   doc.append("# WorkSheet: {}\n".format(path))
@@ -30,7 +57,7 @@ def make_doc(path):
     for func in funcs:
       # print("Function:", func.__name__, func.__doc__) 
       doc.append("### Function: {}".format(func.__name__))
-      doc.append("{}\n".format(func.__doc__))
+      doc.append("{}\n\n".format(func.__doc__))
 
   return doc
 
